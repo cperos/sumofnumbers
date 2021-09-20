03f62157912aeeed0a5f440e74e4976cecf2eb80
Constantine Peros <89947281+cperos@users.noreply.github.com>
2021-09-19 22:06:17 -1000

    Working

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,19 +1,35 @@
-function sumFor
-{
+function sumFor(sumList) {
+  let sum = 0;
 
+  for (let i = 0; i < sumList.length; i++) {
+    sum += sumList[i];
+  }
+  return sum;
 }
 
-function sumWhile
-{
-
+function sumWhile(sumList) {
+  let sum = 0;
+  let i = 0;
+  while (i < sumList.length) {
+    sum += sumList[i];
+    i++;
+  }
+  return sum;
 }
 
-function sumRecursion
-{
-
+function sumRecursion(sumList) {
+  if (sumList.length === 1) return sumList[0]; // base case
+  return sumList[0] + sumRecursion(sumList.slice(1)); // recurse
 }
 
-function sumTheSimpleWay
-{
-
+function sumTheSimpleWay(sumList) {
+  const sum = _.reduce(sumList, function (memo, num) { return memo + num; }, 0);
+  return sum;
 }
+
+const array = [1, 2, 3, 4];
+
+console.log(`Sum For ${sumFor(array)}`);
+console.log(`Sum While ${sumWhile(array)}`);
+console.log(`Sum Recursive ${sumRecursion(array)}`);
+console.log(`Sum Simple ${sumTheSimpleWay(array)}`);
